54275c27f88b8f6bccd3110286563fd1d9aa4e64
chenliujin <liujin.chen@qq.com>
2018-05-16 17:01:41 +0800

    redis.sh

--- docker/redis.sh
+++ docker/redis.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-mkdir -p /var/log/redis/
-touch /var/log/redis/redis.log
-chmod a+w /var/log/redis/redis.log
-
-docker run -d \
+docker run \
+  -d \
 	--restart=always \
-	-v /data/redis:/data \
-	-v /var/log/redis:/var/log/redis \
-	-v /etc/redis.conf:/etc/redis.conf \
-	-p 6679:6379 \
+	-p 6379:6379 \
 	--name redis \
-	redis:3.2.8 \
-	redis-server /etc/redis.conf
+	redis:3.2.10
